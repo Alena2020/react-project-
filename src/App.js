00f1ac72dfd4912550dd7df07827d4ec70940a81
Alena <elenila_skinef@mail.ru>
2024-01-30 22:24:19 +0300

    added Button and Tabs for kalimba

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Header from './components/Header';
-import { cards } from './data';
 import CardToInspire from './components/CardsToInspire'; 
+import Button from './components/Button/Button';
+import { useState } from 'react';
+import { cards, tabs } from './data';
+
 
 
 function App() {
+  const [ content, setContent] = useState('Click Button');
+  function handleClick(type) {
+    setContent(type);
+  }
+
   return (
     <div className="App">
       <Header/>
@@ -19,7 +27,16 @@ function App() {
             <CardToInspire {...cards[3]}/>
             <CardToInspire {...cards[4]}/>
           </ul>
-        </section>        
+        </section>  
+        <section>
+          <h3>Tabs for kalimba</h3>
+          <Button onClick={() => handleClick('witcher')}>Toss a Coin to your Witcher</Button>
+          <Button onClick={() => handleClick('frozen')}>Frozen - Do You Want to Build a Snowman?</Button>
+          <Button onClick={() => handleClick('Priscilla')}>The Witcher 3: Wild Hunt – The Wolven Storm (Priscilla's Song)</Button>
+          <Button onClick={() => handleClick('frog')}>Crazy Frog - Axel F</Button>
+          <Button onClick={() => handleClick('life')}>"It's My Life" - Bon Jovi </Button>
+          <p>{tabs[content]}</p>
+        </section>      
       </main>      
     </div>
   );
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import './Button.css';
+
+function Button({ children, onClick }) {
+
+  return ( 
+    <button className='button active' onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
